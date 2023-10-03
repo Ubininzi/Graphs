5e2ed7e01743cb20c3b47c9658498777da9fcb23
Максим Шабанов <maksim20048.ru@gmail.com>
2023-10-03 19:06:01 +0400

    0

--- Graphs.cs
+++ Graphs.cs
@@ -1,36 +1,54 @@
 ﻿namespace Graphs
 {
-    class Graph
-    {
-        Dictionary<Node, List<Node>> Vertices = new Dictionary<Node, List<Node>>();
+	class Graph
+	{
+		protected Dictionary<Vertex, List<Vertex>> AdjacencyList = new();
+		protected List<Vertex> Vertices = new();
+		protected List<Edge> Edges = new();
 
-        public Graph(Dictionary<Node, List<Node>> vertices)
-        {
-            Vertices = vertices;
-        }
-        public Graph()
-        {
-            Vertices = new Dictionary<Node, List<Node>>();
-        }
-    }
+		public Graph(Dictionary<Vertex, List<Vertex>> vertices)
+		{
+			AdjacencyList = vertices;
+		}
+		public Graph()
+		{
+			AdjacencyList = new Dictionary<Vertex, List<Vertex>>();
+		}
+		public Graph(string path) {
+			int[][] IncidenceMatrix = File.ReadAllText(path).Split("\n").Select(str => str.Split(",").Select(c => Convert.ToInt32(c)).ToArray()).ToArray();
+			Vertices = Enumerable.Range(0,IncidenceMatrix.Length).Select(x => new Vertex(x)).ToList();
 
-    class Node
-    {
-        protected int Id;
-        public Node(int Id)
-        {
-            this.Id = Id;
-        }
-    }
+			//for (int i = 0; i < IncidenceMatrix.Length; i++) {
+			//	for (int j = 0; j < IncidenceMatrix[i].Length; j++){
+			//		int FirstEdge, SecEdge;
+			//		if (IncidenceMatrix[i][j] == 1) {
+			//		}
+			//	}
+			//}
+			createAdjacencyList();
+		}
+		private void createAdjacencyList() { 
+			
+		}
+	}
 
-    class Edge
-    {
-        protected int Id;
-        KeyValuePair<Node, Node> Link;
-        public Edge(int id, KeyValuePair<Node, Node> link)
-        {
-            Id = id;
-            Link = link;
-        }
-    }
+	class Vertex
+	{
+		protected int Id;
+		public Vertex(int Id)
+		{
+			this.Id = Id;
+		}
+	}
+
+	class Edge
+	{
+		protected int Id;
+		protected KeyValuePair<Vertex, Vertex> Link;
+		public Edge(int id, KeyValuePair<Vertex, Vertex> link)
+		{
+			Id = id;
+			Link = link;
+		}
+	}
 }
--- MainProgram.cs
+++ MainProgram.cs
@@ -1,3 +1,3 @@
 ﻿using Graphs;
 
-Graph graph = new Graph();
+Graph graph = new Graph("C:\\Users\\Максим\\source\\repos\\App1\\Graphs\\TestIncidenceMatrix.csv");
