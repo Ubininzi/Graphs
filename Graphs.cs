ee18ee5667219968098273867531ed52d01d6414
Максим Шабанов <maksim20048.ru@gmail.com>
2023-10-04 19:13:11 +0400

    добавление, удаление вершин и граней

--- Graphs.cs
+++ Graphs.cs
@@ -1,11 +1,12 @@
-﻿namespace Graphs
+﻿using System.Linq;
+
+namespace Graphs
 {
 	class Graph
 	{
-		protected Dictionary<Vertex, List<Vertex>> AdjacencyList = new();
-		protected List<Vertex> Vertices = new();
-		protected List<Edge> Edges = new();
-
+		internal Dictionary<Vertex, List<Vertex>> AdjacencyList = new();
+		//List<Vertex> Vertices = new();
+		//List<Edge> Edges = new();
 		public Graph(Dictionary<Vertex, List<Vertex>> vertices)
 		{
 			AdjacencyList = vertices;
@@ -16,7 +17,7 @@
 		}
 		public Graph(string path) {
 			int[][] IncidenceMatrix = File.ReadAllText(path).Split("\n").Select(str => str.Split(",").Select(c => Convert.ToInt32(c)).ToArray()).ToArray();
-			Vertices = Enumerable.Range(0,IncidenceMatrix.Length).Select(x => new Vertex(x)).ToList();
+			//Vertices = Enumerable.Range(0,IncidenceMatrix.Length).Select(x => new Vertex(x.ToString())).ToList();
 
 			//for (int i = 0; i < IncidenceMatrix.Length; i++) {
 			//	for (int j = 0; j < IncidenceMatrix[i].Length; j++){
@@ -25,17 +26,45 @@
 			//		}
 			//	}
 			//}
-			createAdjacencyList();
+			CreateAdjacencyList();
 		}
-		private void createAdjacencyList() { 
-			
+		public void AddVertex() {
+			//проверка на дубликат
+			Console.WriteLine("Введите название вершины");
+			string id = Console.ReadLine();
+			AdjacencyList.Add(new Vertex(id), new List<Vertex>());
+			Console.WriteLine($"вы создали вершину {id}");
+		}
+		public void RemoveVertex(string id) {
+			//AdjacencyList.Values.Select(x => x.Remove(GetVertexId(id)));
+			foreach (var vertices in AdjacencyList.Values) vertices.Remove(GetVertexId(id));
+			AdjacencyList.Remove(GetVertexId(id));
+		}
+		public void AddEdge(string id1, string id2) {
+			if (id1 == id2) AdjacencyList[GetVertexId(id1)].Add(GetVertexId(id2));
+			else
+			{
+				AdjacencyList[GetVertexId(id1)].Add(GetVertexId(id2));
+				AdjacencyList[GetVertexId(id2)].Add(GetVertexId(id1));
+			}
+		}
+		public void RemoveEdge(string id1, string id2) {
+			AdjacencyList[GetVertexId(id2)].Remove(GetVertexId(id1));
+			AdjacencyList[GetVertexId(id1)].Remove(GetVertexId(id2));
+        }
+        private void CreateAdjacencyList() {
+			CreateEdgeList();
+		}
+		private void CreateEdgeList() { }
+		private Vertex GetVertexId(string id) {
+			return AdjacencyList.Keys.First(x => x.Id == id);
 		}
 	}
 
 	class Vertex
 	{
-		protected int Id;
-		public Vertex(int Id)
+		internal string Id;
+		public Vertex(string Id)
 		{
 			this.Id = Id;
 		}
@@ -43,12 +72,13 @@
 
 	class Edge
 	{
-		protected int Id;
-		protected KeyValuePair<Vertex, Vertex> Link;
+		internal int Id;
+		internal KeyValuePair<Vertex, Vertex> Link;
 		public Edge(int id, KeyValuePair<Vertex, Vertex> link)
 		{
 			Id = id;
 			Link = link;
 		}
 	}
+
 }
