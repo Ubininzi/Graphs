59cddf5f44000ab8e7039b3aea2aea2c68a957b8
Максим Шабанов <maksim20048.ru@gmail.com>
2023-10-11 18:16:22 +0400

    task2

--- MainProgram.cs
+++ MainProgram.cs
@@ -1,8 +1,12 @@
 ﻿using Graphs;
 
-Graph graph = new("C:\\Users\\Максим\\source\\repos\\App1\\Graphs\\TestAdjacencyMatrix.csv");
+Graph graph1 = new("C:\\Users\\Максим\\source\\repos\\App1\\Graphs\\AdjacencyMatrix2.csv");
+Graph graph2 = new("C:\\Users\\Максим\\source\\repos\\App1\\Graphs\\TestAdjacencyMatrix.csv");
 //Graph graph = new("D:\\Users\\st315-09\\source\\repos\\GRPHS\\TestAdjacencyMatrix.csv");
-GraphUI.UI(graph);
+GraphUI.UI(graph1);
+GraphUI.UI(graph2);
 
-Graph Kgraph = Graph.ComplementGraph(graph);
-GraphUI.UI(Kgraph);
+//Graph Kgraph = Graph.ComplementGraph(graph1);
+Graph Ugraph = Graph.IntersectionGraph(graph1,graph2);
+if (Ugraph != null )
+GraphUI.UI(Ugraph);
