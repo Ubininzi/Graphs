75e66ed6a2d7e249b88955d9550615332e9b0b50
ST306-06 <ST306-06@W19-306-06>
2023-10-13 14:17:27 +0400

    deleted class vertex

--- MainProgram.cs
+++ MainProgram.cs
@@ -1,12 +1,14 @@
 ﻿using Graphs;
 
-Graph graph1 = new("C:\\Users\\Максим\\source\\repos\\App1\\Graphs\\AdjacencyMatrix2.csv");
-Graph graph2 = new("C:\\Users\\Максим\\source\\repos\\App1\\Graphs\\TestAdjacencyMatrix.csv");
-//Graph graph = new("D:\\Users\\st315-09\\source\\repos\\GRPHS\\TestAdjacencyMatrix.csv");
+//Graph graph1 = new("C:\\Users\\Максим\\source\\repos\\App1\\Graphs\\AdjacencyMatrix2.csv");
+//Graph graph2 = new("C:\\Users\\Максим\\source\\repos\\App1\\Graphs\\TestAdjacencyMatrix.csv");
+Graph graph1 = new("D:\\Users\\st306-06\\source\\repos\\graphs\\TestAdjacencyMatrix.csv");
+Graph graph2 = new("D:\\Users\\st306-06\\source\\repos\\graphs\\TestAdjacencyMatrix.csv");
+
 GraphUI.UI(graph1);
 GraphUI.UI(graph2);
 
 //Graph Kgraph = Graph.ComplementGraph(graph1);
-Graph Ugraph = Graph.IntersectionGraph(graph1,graph2);
-if (Ugraph != null )
-GraphUI.UI(Ugraph);
+Graph Ugraph = Graph.IntersectionGraph(graph1, graph2);
+if (Ugraph != null)
+    GraphUI.UI(Ugraph);
